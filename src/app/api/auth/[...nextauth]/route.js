8f92ccaf450d15f2275204d74afd0ac6d38be76f
[name] <jaipal@createbytes.com>
2023-10-06 15:45:39 +0530

    added git secret in NEXTAUTH

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,16 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "../../../variables.ts";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET,NEXTAUTH_SECRET } from "../../../variables.ts";
 
 const handler = NextAuth({
     providers : [
         GoogleProvider({
             clientId : GOOGLE_CLIENT_ID,
-            clientSecret : GOOGLE_CLIENT_SECRET
+            clientSecret : GOOGLE_CLIENT_SECRET,
+            
         })
-    ]
+    ],
+    secret : NEXTAUTH_SECRET
 })
 
 export { handler as GET, handler as POST }
--- src/app/variables.ts
+++ src/app/variables.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_CLIENT_ID : string = '331446831986-7kveou16mfr99f400sujen6n7073b5l9.apps.googleusercontent.com'
 export const GOOGLE_CLIENT_SECRET : string = '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';
-
+export const NEXTAUTH_SECRET : string = 'MY_NEXTAUTH_SECRET'
 export const NEXTAUTH_URL : string = 'https://next-employee-app.vercel.app';
 
 export const AUTHENTICATED = 'authenticated';
