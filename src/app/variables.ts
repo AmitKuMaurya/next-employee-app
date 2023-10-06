eb544f3ff1256880edfdbf4b799a51f13f0fe0c0
[name] <jaipal@createbytes.com>
2023-10-06 14:26:10 +0530

    changed nextauth_url to deployed url

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -1,7 +1,7 @@
 export const GOOGLE_CLIENT_ID : string = '331446831986-7kveou16mfr99f400sujen6n7073b5l9.apps.googleusercontent.com'
 export const GOOGLE_CLIENT_SECRET : string = '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';
 
-export const NEXTAUTH_URL : string = 'http://localhost:3000';
+export const NEXTAUTH_URL : string = 'https://next-employee-app.vercel.app';
 
 export const AUTHENTICATED = 'authenticated';
 export const UNAUTHENTICATED = 'unauthenticated';
